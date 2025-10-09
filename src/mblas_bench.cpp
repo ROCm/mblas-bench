f9af25225da12b11a34494eca92644dc307ae71d
T.Kurotobi <tkurotob@amd.com>
2025-10-09 16:56:38 +0000

    removed unnecessary variables

--- src/mblas_bench.cpp
+++ src/mblas_bench.cpp
@@ -265,23 +265,19 @@ int main(int argc, char **argv) {
     // Select backend implementation
     string driver = s_to_lower(result["driver"].as<string>());
     string function = s_to_lower(result["function"].as<string>());
-    int requested_solution_num = result["requested_solution_num"].as<int>();
 
     if (driver == "cublaslt" || (driver == "cublas" && function == "matmul")) {
       // Since regular cublas has no matmul, we can safely assume the user means
       // cublaslt
       gemm = new cublaslt_gemm_factory();
     } else if (driver == "cublas-bench" || driver == "cublas") {
-      requested_solution_num = 1; // cublas only returns one solution
       gemm = new cublas_gemm_factory();
     } else if (driver == "hipblaslt" || (driver == "rocblas" && function == "matmul")) {
       // Since regular rocblas has no matmul, we can safely assume the user means
       // hipblaslt
       // gemm = new hipblaslt_gemm(result);
-      requested_solution_num = 1; // This feature is not implemented yet
       gemm = new hipblaslt_gemm_factory();
     } else if (driver == "rocblas-bench" || driver == "rocblas") {
-      requested_solution_num = 1; // rocblas only returns one solution
       gemm = new rocblas_gemm_factory();
     } else {
       cerr << "Driver \"" << driver << "\" not supported" << endl;
